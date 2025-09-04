b92f79ab2fd4378a0c3f8583dae596b3f1e53e54
David Scholer <davidascholer@gmail.com>
2025-09-04 16:57:19 -0700

    edit auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,40 +3,45 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 const { verify } = jwt;
 import prisma from "../prismaClient";
+import { userReturnSelect } from "../lib/returnSignatures";
 
 export interface AuthenticatedRequest extends Request {
   user?: User;
 }
 
-export const authenticate = async (
+export const authenticateUser = async (
   req: AuthenticatedRequest,
   res: Response,
   next: NextFunction
-): Promise<void> => {
+) => {
   if (
     !req.headers.authorization ||
     !req.headers.authorization.startsWith("Bearer ")
   ) {
-    throw new Error("Unauthorized");
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   const token = req.headers.authorization.split(" ")[1];
   if (!token) {
-    throw new Error("Token not found");
+    return res.status(401).json({ message: "Token not found" });
   }
 
   try {
-    const decode = verify(token, process.env.JWT_SECRET || "") as {
-      email: string;
-      id: string;
-    };
+      const decode = verify(token, process.env.JWT_SECRET || "") as {
+        email: string;
+        id: string;
+      };
+  
     const user = await prisma.user.findUnique({
       where: { email: decode.email },
+      select: userReturnSelect,
     });
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
     req.user = user ?? undefined;
     next();
   } catch (error) {
-    req.user = undefined;
-    next();
+    res.status(401).json({ message: "Unauthorized" });
   }
 };
